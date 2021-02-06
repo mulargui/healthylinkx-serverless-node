899cf170c477a45b97100c05e4cbd0ed25dbd35b
mulargui <mauricio@ulargui.com>
2021-02-06 12:29:43 -0800

    Testing aws-cli

--- infra/src/index.ts
+++ infra/src/index.ts
@@ -1,42 +1,4 @@
-const constants = require('./envparams.ts');
-const {
-	S3Client,
-	PutObjectCommand,
-	CreateBucketCommand
-} = require("@aws-sdk/client-s3");
+const commands = require('./S3Create.ts');
 
-// Set the AWS region and secrets
-const config = {
-	accessKeyId: constants.AWS_ACCESS_KEY_ID, 
-	secretAccessKey: constants.AWS_SECRET_ACCESS_KEY, 
-	region: constants.AWS_REGION
-};
-
-
-// Set the bucket parameters
-const bucketName = "healthylinkx";
-const bucketParams = { Bucket: bucketName };
-
-// Create name for uploaded object key
-const keyName = "hello_world.txt";
-const objectParams = { Bucket: bucketName, Key: keyName, Body: "Hello World!" };
-
-// Create an S3 client service object
-const s3 = new S3Client(config);
-
-const run = async () => {
-	// Create S3 bucket
-	try {
-		const data = await s3.send(new CreateBucketCommand(bucketParams));
-		console.log("Success. Bucket created.");
-	} catch (err) {
-		console.log("Error: ", err);
-	}
-	try {
-		const results = await s3.send(new PutObjectCommand(objectParams));
-		console.log("Successfully uploaded data to " + bucketName + "/" + keyName);
-	} catch (err) {
-		console.log("Error: ", err);
-	}
-};
-run();
+console.log(process.argv);
+commands.S3Create();
