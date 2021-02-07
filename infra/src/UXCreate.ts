20a4dfb61e8f448e3c9f99677be8f69031ff0107
mulargui <mauricio@ulargui.com>
2021-02-06 19:34:27 -0800

    creating ux

--- infra/src/UXCreate.ts
+++ infra/src/UXCreate.ts
@@ -20,7 +20,7 @@ const bucketName = "healthylinkx";
 const directoryToUpload = '/home/cloudshell-user/healthylinkx-serverless-node/ux/src';
 
 // ======= helper functions ==========
-async function walkSync(currentDirPath, callback) {
+async function walkSync(currentDirPath, async callback) {
 	fs.readdirSync(currentDirPath).forEach(function (name) {
 		var filePath = path.join(currentDirPath, name);
 		var stat = fs.statSync(filePath);
