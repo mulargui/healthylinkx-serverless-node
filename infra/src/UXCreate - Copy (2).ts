18ad82edb4ca963601b506cca85362cc0caeb4b3
mulargui <mauricio@ulargui.com>
2021-02-06 18:23:02 -0800

    creating ux

--- infra/src/UXCreate - Copy (2).ts
+++ infra/src/UXCreate - Copy (2).ts
@@ -6,7 +6,6 @@ const {
 } = require("@aws-sdk/client-s3");
 const fs = require('fs');
 const path = require('path');
-//const readdir = require("recursive-readdir");
 
 // Set the AWS region and secrets
 const config = {
@@ -19,42 +18,35 @@ const config = {
 const bucketName = "healthylinkx";
 const bucketParams = { Bucket: bucketName };
 
-const directoryToUpload = '/home/cloudshell-user/healthylinkx-serverless-node/ux/src';
+//helper function to upload a dir to S3
+async function uploadDir(s3Path: string, bucketName: string) {
+  const s3 = new S3();
 
-// ===== helper functions =======
-// get file paths
-const filePaths = [];
-const getFilePaths = (dir) => {
-  fs.readdirSync(dir).forEach(function (name) {
-    const filePath = path.join(dir, name);
-    const stat = fs.statSync(filePath);
-    if (stat.isFile()) {
-      filePaths.push(filePath);
-    } else if (stat.isDirectory()) {
-      getFilePaths(filePath);
-    }
-  });
-};
+  // Recursive getFiles from
+  // https://stackoverflow.com/a/45130990/831465
+  async function getFiles(dir: string): Promise<string | string[]> {
+    const dirents = await fs.readdir(dir, { withFileTypes: true });
+    const files = await Promise.all(
+      dirents.map((dirent) => {
+        const res = path.resolve(dir, dirent.name);
+        return dirent.isDirectory() ? getFiles(res) : res;
+      })
+    );
+    return Array.prototype.concat(...files);
+  }
 
-// upload to S3
-const uploadToS3 = (s3, dir, path) => {
-  return new Promise((resolve, reject) => {
-    const key = path.split(`${dir}/`)[1];
-    const params = {
-      Bucket: bucketName,
-      Key: key,
-      Body: fs.readFileSync(path),
-    };
-    s3.putObject(params, (err) => {
-      if (err) {
-        reject(err);
-      } else {
-        console.log(`uploaded ${params.Key} to ${params.Bucket}`);
-        resolve(path);
-      }
-    });
-  });
-};
+  const files = (await getFiles(s3Path)) as string[];
+  const uploads = files.map((filePath) =>
+    s3
+      .putObject({
+        Key: path.relative(s3Path, filePath),
+        Bucket: bucketName,
+        Body: fs.createReadStream(filePath),
+      })
+      .promise()
+  );
+  return Promise.all(uploads);
+}
 
 async function UXCreate() {
 	// Create an S3 client service object
@@ -67,20 +59,13 @@ async function UXCreate() {
 	} catch (err) {
 		console.log("Error: ", err);
 	}
-
-	getFilePaths(directoryToUpload);
-
-const uploadPromises = filePaths.map((path) =>
-  uploadToS3(AWSs3Client, directoryToUpload, path)
-);
-
-Promise.all(uploadPromises)
-  .then((result) => {
-    console.log('uploads complete');
-    console.log(result);
-  })
-  .catch((err) => console.error(err));
-
+	
+	try {
+		await uploadDir(path.resolve("/home/cloudshell-user/healthylinkx-serverless-node/ux/src"), bucketName);
+		console.log("Success. Files uploaded.");
+	} catch (err) {
+		console.log("Error: ", err);
+	}
 }
 
 /*
