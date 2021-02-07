a82413f6947486e267a9369eb29d5a1a25fae420
mulargui <mauricio@ulargui.com>
2021-02-06 20:06:04 -0800

    creating ux

--- infra/src/UXCreate.ts
+++ infra/src/UXCreate.ts
@@ -62,7 +62,7 @@ async function UXCreate() {
 	const staticHostParams = {
 		Bucket: bucketName,
 		WebsiteConfiguration: {
-			ErrorDocument: {Key: "index.html"},
+			ErrorDocument: {Key: ""},
 			IndexDocument: {Suffix: "index.html"}
 		}
 	};
