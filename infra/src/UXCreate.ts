7e0de86e54d006927662263446be17b87f29f7b3
mulargui <mauricio@ulargui.com>
2021-02-12 19:03:25 -0800

    UXCreate

--- infra/src/UXCreate.ts
+++ infra/src/UXCreate.ts
@@ -54,7 +54,7 @@ sed "s/APIID/$APIID/" $ROOT/ux/src/js/constants.template.js > $ROOT/ux/src/js/co
 	//copy files
     walkSync(directoryToUpload, async function(filePath, stat) {
         let bucketPath = filePath.substring(directoryToUpload.length+1);
-        let params = {Bucket: bucketName, Key: bucketPath, Body: fs.readFileSync(filePath), ACL:'public-read'};
+        let params = {Bucket: bucketName, Key: bucketPath, Body: fs.readFileSync(filePath), ContentType: 'text/html', ACL:'public-read'};
  
 		try {
 			const data = await AWSs3Client.send(new PutObjectCommand(params));
