9192402c8ad2c684337d32bf7cd0d3a6aa5063f9
mulargui <mauricio@ulargui.com>
2021-02-13 19:50:58 -0800

    DSCreate

--- infra/src/DSCreate.ts
+++ infra/src/DSCreate.ts
@@ -93,15 +93,9 @@ async function DSCreate() {
 			var percent = Math.floor(progress.bytes_processed / progress.total_bytes * 10000) / 100;
 			console.log(`${percent}% Completed`);
 		});
-
-		importer.import(constants.ROOT + '/datastore/src/healthylinkxdump.sql')
-			.then(()=>{
-				var files_imported = importer.getImported();
-				console.log(`${files_imported.length} SQL file(s) imported.`);
-			}).catch(err=>{
-				console.error(err);
-			});
-
+		await importer.import(constants.ROOT + '/datastore/src/healthylinkxdump.sql');
+		console.log("Success. healthylinkx-db populated with data.");
+		
 		//delete the unzipped file
 		fs.unlinkSync(constants.ROOT + '/datastore/src/healthylinkxdump.sql');
 		
