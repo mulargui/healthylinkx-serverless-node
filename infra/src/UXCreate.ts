667a6cf68b85083953d6031d1972f57d8babe275
mulargui <mauricio@ulargui.com>
2021-02-21 16:49:33 -0800

    UX constants

--- infra/src/UXCreate.ts
+++ infra/src/UXCreate.ts
@@ -5,6 +5,10 @@ const {
 	CreateBucketCommand,
 	PutBucketWebsiteCommand
 } = require("@aws-sdk/client-s3");
+const {
+    APIGatewayClient,
+    GetRestApisCommand
+} = require("@aws-sdk/client-api-gateway");
 const fs = require('fs');
 const path = require('path');
 
@@ -42,17 +46,38 @@ sed "s/APIID/$APIID/" $ROOT/ux/src/js/constants.template.js > $ROOT/ux/src/js/co
 */
 	
 	try {
+		
+		//we need to set the url of the api in the constants.js file
+		//URL of the api
+		const apigwclient = new APIGatewayClient(config);
+		var data = await apigwclient.send(new GetRestApisCommand({}));
+		console.log(data);
+		return;
+		const endpointid = data.id;
+		console.log("API endpoint: " + endpointid);
+
+		// create contants.js with env values
+		fs.copyFileSync(constants.ROOT+'/ux/src/js/constants.template.js', constants.ROOT+'/ux/src/js/constants.js');
+		const options = {
+			files: constants.ROOT+'/ux/src/js/constants.js',
+			from: ['APIID', 'REGION'],
+			to: [endpointid, constants.AWS_REGION]
+		};
+		await replace(options);
+		console.log("Success. Constants updated.");
+		
 		// Create an S3 client service object
 		const AWSs3Client = new S3Client(config);
 
 		// Create S3 bucket
-		var data = await AWSs3Client.send(new CreateBucketCommand({ Bucket: bucketName }));
+		await AWSs3Client.send(new CreateBucketCommand({ Bucket: bucketName }));
 		console.log("Success. " + bucketName + " bucket created.");
 
 		//copy files
 		walkSync(directoryToUpload, async function(filePath, stat) {
 			let bucketPath = filePath.substring(directoryToUpload.length+1);
-			let params = {Bucket: bucketName, Key: bucketPath, Body: fs.readFileSync(filePath), ContentType: 'text/html', ACL:'public-read'};
+			let params = {Bucket: bucketName, Key: bucketPath, Body: fs.readFileSync(filePath), 
+				ContentType: 'text/html', ACL:'public-read'};
 
 			//associate the content type related to the file extension
 			switch (path.extname(bucketPath)) {
@@ -79,7 +104,7 @@ sed "s/APIID/$APIID/" $ROOT/ux/src/js/constants.template.js > $ROOT/ux/src/js/co
 				break;
 			}
 
-			data = await AWSs3Client.send(new PutObjectCommand(params));
+			await AWSs3Client.send(new PutObjectCommand(params));
 			console.log("Success. " + bucketPath + " file copied to bucket " + bucketName);
 		});
 	
@@ -91,10 +116,11 @@ sed "s/APIID/$APIID/" $ROOT/ux/src/js/constants.template.js > $ROOT/ux/src/js/co
 			}
 		};
 
-		data = await AWSs3Client.send(new PutBucketWebsiteCommand(staticHostParams));
+		await AWSs3Client.send(new PutBucketWebsiteCommand(staticHostParams));
 		console.log("Success. " + bucketName + " setup as a static web.");
 		
 		console.log("URL of the bucket: http://" + bucketName + ".s3-website-" + constants.AWS_REGION + ".amazonaws.com/");
+		
 	} catch (err) {
 		console.log("Error. ", err);
 	}
